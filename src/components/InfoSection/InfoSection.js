2b178cd8313c1adb4134b1da79bfdea3d308db62
Habib Amna <habibamna14@gmail.com>
2020-12-17 21:25:35 +0700

    First commit , footer WIP

---
+++ src/components/InfoSection/InfoSection.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Container , Button} from '../../globalStyles';
+import {
+    InfoSec, 
+    InfoRow, 
+    InfoColumn, 
+    TextWrapper, 
+    TopLine, 
+    Heading, 
+    Subtitle,
+    ImgWrapper,
+    Img
+} from './InfoSection.elements';
+
+const infoSection = ({ primary, lightBg ,imgStart, lightTopLine, lightTextDesc , buttonlabel, description, headline, lightText, topLine, img, alt, start}) => {
+    return (
+        <>
+            <InfoSec lightBg={lightBg}>
+                <Container>
+                    <InfoRow imgStart={imgStart}>
+                        <InfoColumn>
+                            <TextWrapper>
+                                <TopLine lightTopLine={lightTopLine}>{topLine}</TopLine>
+                                <Heading lightText={lightText}>{headline}</Heading>
+                                <Subtitle lightTextDesc={lightTextDesc}>{description}</Subtitle>
+                                <Link to='/sign-up'>
+                                    <Button big fontBig primary={primary}>
+                                        {buttonlabel}
+                                    </Button>
+                                </Link>
+                            </TextWrapper>
+                        </InfoColumn>
+                        <InfoColumn>
+                            <ImgWrapper start={start}>
+                                <Img src={img} alt={alt} />
+                            </ImgWrapper>
+                        </InfoColumn>
+                    </InfoRow>
+                </Container>
+            </InfoSec>
+        </>
+    );
+};
+
+export default infoSection;
---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,47 @@
+export const homeObjOne = {
+    lightBg:true,
+    primary: false,
+    imgStart:'', 
+    lightTopLine: false, 
+    lightTextDesc:false , 
+    buttonlabel: 'Get Started', 
+    description:'We help your diet plan and workout to be healthy and muscular . Our diet plan can help you achieve your body and muscle goals.', 
+    headline: 'Lead The Diet Plan for Your Body Goals', 
+    lightText: false, 
+    topLine:'Healthy Life',
+    img: require('../../image/working.svg').default, 
+    alt: 'Gambar', 
+    start: 'true'
+};
+
+export const homeObjTwo = {
+    lightBg:false,
+    primary: true,
+    imgStart:'start', 
+    lightTopLine: true, 
+    lightTextDesc:true , 
+    buttonlabel: 'Get Started', 
+    description:'We help your diet plan and workout to be healthy and muscular . Our diet plan can help you achieve your body and muscle goals.', 
+    headline: 'Lead The Diet Plan for Your Body Goals', 
+    lightText: true, 
+    topLine:'Healthy Life',
+    img: require('../../image/sehat.svg').default, 
+    alt: 'Gambar', 
+    start: ''
+};
+
+export const homeObjThree = {
+    lightBg:true,
+    primary: false,
+    imgStart:'', 
+    lightTopLine: false, 
+    lightTextDesc:false , 
+    buttonlabel: 'Get Started', 
+    description:'We help your diet plan and workout to be healthy and muscular . Our diet plan can help you achieve your body and muscle goals.', 
+    headline: 'Lead The Diet Plan for Your Body Goals', 
+    lightText: false, 
+    topLine:'Healthy Life',
+    img: require('../../image/tracker.svg').default, 
+    alt: 'Gambar', 
+    start: 'true'
+};
